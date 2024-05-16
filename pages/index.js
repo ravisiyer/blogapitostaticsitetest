8640c964b35c56502980712e4bec9b7f35f5b387
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-05-16 18:43:21 +0530

    5 Post titles shown

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
-export default function Home() {
-  return <p>Hi there!</p>;
+const BLOGPOSTSREQ =
+  "https://www.googleapis.com/blogger/v3/blogs/5765625164803845699/posts?maxResults=5&key=";
+
+export default function Home({ postItems }) {
+  return (
+    <div>
+      {postItems ? (
+        <ul>
+          {postItems.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>postItems is false (null or undefined)</p>
+      )}
+    </div>
+  );
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const blogReqURL = BLOGPOSTSREQ + process.env.APIKEY;
+  const res = await fetch(blogReqURL);
+  const response = await res.json();
+
+  const postItems = response.items;
+
+  // By returning { props: { postItems } }, the Blog component
+  // will receive `postItems` as a prop at build time
+  return {
+    props: {
+      postItems,
+    },
+  };
 }
